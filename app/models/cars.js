4bd93cf526798d7aa284403d77b0f1ff77d18248
Andien Khansa'a <khansaandien@gmail.com>
2022-06-06 09:46:58 +0700

    update backend

--- app/models/cars.js
+++ app/models/cars.js
@@ -2,11 +2,6 @@
 const {Model} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
     class Cars extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
             // define association here
         }
@@ -20,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.STRING,
                 get() {
                     const rawValue = this.getDataValue("image");
-                    return rawValue ? "http://localhost:8000" + rawValue.substring(1) : null;
+                    return rawValue ? "https://challange-7-tim2-be.herokuapp.com" + rawValue.substring(1) : null;
                 },
             },
             rentPerDay: DataTypes.INTEGER,
--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
-require("dotenv").config();
-
 const {DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 module.exports = {
     development: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
-        database: `${DB_NAME}`,
+        database: DB_NAME,
         host: DB_HOST,
         dialect: "postgres",
         ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     },
     test: {
         username: DB_USERNAME,
@@ -18,6 +22,12 @@ module.exports = {
         host: DB_HOST,
         dialect: "postgres",
         ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     },
     production: {
         username: DB_USERNAME,
